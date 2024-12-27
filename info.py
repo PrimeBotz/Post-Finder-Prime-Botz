bcfe87087aa78258834fd4f9bd59ef10f841979d
PrimeBotz <primemovies4unayem@gmail.com>
2024-12-27 22:29:21 +0600

    New Update 🥰 By TG @Prime_Botz

--- info.py
+++ info.py
@@ -5,3 +5,9 @@ SESSION      = ""
 DATABASE_URI = ""
 LOG_CHANNEL  = -100
 ADMIN        = 
+
+
+# Don't Remove Credit Tg - https://t.me/Prime_Botz
+# Subscribe Telegram Channel For Amazing Bot https://t.me/Prime_Botz
+# Support Group Tg ➠ https://t.me/Prime_Botz_Support
+# Ask Doubt on HTTPS://T.ME/MR_PRIME_SUPREME
