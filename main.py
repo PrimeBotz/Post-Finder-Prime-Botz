bfd13a1f567dcee2bb5de4e55e8fd85ce85bc094
PrimeBotz <primemovies4unayem@gmail.com>
2024-12-27 22:28:52 +0600

    New Update 🥰 By TG @Prime_Botz

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from client import Bot
 
 print("Prime Post Finder Bot Started 💥")
 Bot().run()
+
+# Don't Remove Credit Tg - https://t.me/Prime_Botz
+# Subscribe Telegram Channel For Amazing Bot https://t.me/Prime_Botz
+# Support Group Tg ➠ https://t.me/Prime_Botz_Support
+# Ask Doubt on HTTPS://T.ME/MR_PRIME_SUPREME
+
